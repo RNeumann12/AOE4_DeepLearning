6a057d917b92095841dad21772fa340fe0aa2f1e
RNeumann12 <rineumann@edu.aau.at>
2026-03-03 17:32:05 +0100

    - Cleanup of WinRatePredction - Improvements for inference

---
+++ WinRatePrediction/__init__.py
@@ -0,0 +1,8 @@
+"""WinRatePrediction package
+
+This file marks the directory as a python package so modules like
+`WinRatePrediction.WinRateTransformerModel` can be imported when
+running scripts from the repository root.
+"""
+
+__all__ = ["WinRateTransformerModel"]
